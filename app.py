a31bf55975d0ca4193ba59754bb6d86380aa114d
Miguel Rueda <mjruedap2003@gmail.com>
2026-08-20 10:04:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 from PIL import Image
 st.title("HOLA !!! mi Nombre es Miguelato ")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
+image = Imagen.open('Juan_imagen_10.jpg')
+st.image(image, caption='Interfaces multimodales')
