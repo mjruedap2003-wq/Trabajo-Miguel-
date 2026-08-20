6e28443fcb3edcc818aae29297d5eb07560352c1
Miguel Rueda <mjruedap2003@gmail.com>
2026-08-20 10:08:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ st.title("HOLA !!! mi Nombre es Miguelato ")
 
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
 st.write("Facilmente puedo realizar backend y frontend.")
-image = Imagen.open('Juan_imagen_10.jpg')
+image = Image.open('Juan_imagen_10.jpg')
 st.image(image, caption='Interfaces multimodales')
